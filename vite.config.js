81bb621dc0ff2976473376970242b1a4ba087704
Aftab <md.aftab5295@gmail.com>
2026-09-11 11:46:40 +0530

    Restructure project into frontend and backend

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'src/frontend',
   plugins: [react()],
+  publicDir: '../../public',
+  build: {
+    outDir: '../../dist',
+    emptyOutDir: true,
+  },
 })
